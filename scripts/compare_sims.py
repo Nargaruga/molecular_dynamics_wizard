45f4faa08d3011458648064d2e421d917a178390
Leonardo Da Pozzo <l.dapozzo@studenti.unipi.it>
2025-03-02 22:15:55 +0100

    use mdanalysis for rmsd

--- scripts/compare_sims.py
+++ scripts/compare_sims.py
@@ -5,57 +5,42 @@ import json
 import numpy as np
 from matplotlib.axes import Axes
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
 
 from pymol import cmd
 import MDAnalysis as mda  # TODO cite the use of rms and align as required by (https://userguide.mdanalysis.org/stable/examples/analysis/alignment_and_rms/rmsf.html)
 from MDAnalysis.analysis import rms, align
 
+buttons = []
 
-def compute_rmsd_by_frame(
-    mobile: str, target: str, frames: int, is_static: bool = False
-):
-    rmsd = []
-    for frame in range(1, frames + 1):
-        rmsd.append(
-            cmd.align(
-                mobile,
-                target,
-                cycles=0,
-                mobile_state=frame,
-                target_state=1 if is_static else frame,
-            )[0]
-        )
-    return rmsd
-
-
-def average_rmsd_by_frame(runs: list[list[int]], n_frames: int) -> list[int]:
+def average_rmsd(runs, n_frames: int) -> list[int]:
     if len(runs) < 1:
         return []
 
     avg_rmsd = []
     for frame in range(0, n_frames):
-        avg_rmsd.append(sum([run[frame] for run in runs]) / len(runs))
+        avg_rmsd.append(sum([run.results.rmsd[frame][2] for run in runs]) / len(runs))
 
     return avg_rmsd
 
 
 def plot_rmsd(
     ax: Axes,
-    rmsd_by_frame_by_depth: list[list[int]],
+    rmsd_by_depth: list[list[int]],
     str_depths: list[str],
     n_frames: int,
 ):
-    for rmsd_by_frame in rmsd_by_frame_by_depth:
+    for rmsd in rmsd_by_depth:
         ax.plot(
             [x + 1 for x in range(0, n_frames)],
-            rmsd_by_frame,
+            rmsd,
         )
     ax.legend(str_depths, title="Neighbourhood depth")
     ax.set_xlabel("Frame")
     ax.set_ylabel("RMSD (Å)")
 
 
-def compute_rmsf(topology, trajectory, residues):
+def compute_rmsf(topology, trajectory, selection_str):
     u = mda.Universe(topology, trajectory)
 
     average = align.AverageStructure(
@@ -65,9 +50,6 @@ def compute_rmsf(topology, trajectory, residues):
     ref = average.results.universe
     align.AlignTraj(u, ref, select="protein and name CA", in_memory=True).run()
 
-    selection_str = " or ".join(
-        [f"(resid {res} and chainID {chain})" for res, chain in residues]
-    )
     c_alphas = u.select_atoms(f"protein and name CA and ({selection_str})")
 
     rmsf = rms.RMSF(c_alphas).run()
@@ -92,12 +74,32 @@ def plot_rmsf(
 ):
     for rmsf, c_alphas in rmsf_by_depth:
         ax.plot(c_alphas.resids, rmsf, linestyle="--", marker="o")
-        for xy in zip(c_alphas.resids, rmsf):
-            ax.annotate("(%s, %.2f)" % xy, xy=xy, textcoords="data")
+
+    annotate_rmsf(ax, rmsf_by_depth)
 
     ax.legend(str_depths, title="Neighbourhood depth")
     ax.set_xlabel("Residue ID")
     ax.set_ylabel("RMSF (Å)")
+
+
+def annotate_rmsf(ax: Axes, rmsf_by_depth):
+    for rmsf, c_alphas in rmsf_by_depth:
+        for xy in zip(c_alphas.resids, rmsf):
+            ax.annotate("(%s, %.2f)" % xy, xy=xy, textcoords="data")
+
+
+def remove_annotations(ax: Axes):
+    for annotation in ax.texts:
+        annotation.remove()
+
+
+def toggle_annotation(ax: Axes, rmsf_by_depth):
+    if ax.texts:
+        remove_annotations(ax)
+    else:
+        annotate_rmsf(ax, rmsf_by_depth)
+
+    plt.draw()
 
 
 def plot_duration(ax: Axes, entries: list[int], depths: list[int]):
@@ -112,10 +114,13 @@ def plot_duration(ax: Axes, entries: list[int], depths: list[int]):
 
 
 def process_directory(
-    sel_on_minimized: str,
+    full_sim,
+    minimized,
     dir: str,
     depth: int,
-    paratope_residues: list[tuple[int, str]],
+    paratope_selection: str,
+    paratope_hc_selection: str,
+    paratope_lc_selection: str,
     frames: int,
 ):
     print(f"Processing simulations of depth {depth}")
@@ -126,16 +131,6 @@ def process_directory(
     rmsf_h_runs = []
     rmsf_l_runs = []
 
-    partial_sim = f"partial_sim_d{depth}"
-    sel_on_partial = f"paratope_d{depth}"
-
-    paratope_hc_residues = [
-        (resi, chain) for (resi, chain) in paratope_residues if chain == "H"
-    ]
-    paratope_lc_residues = [
-        (resi, chain) for (resi, chain) in paratope_residues if chain == "L"
-    ]
-
     # process all simulations performed with this neighbourhood depth
     sim_dirs = [f.path for f in os.scandir(dir) if f.is_dir() and f.name != "ignore"]
     for sim_dir in sim_dirs:
@@ -145,50 +140,40 @@ def process_directory(
             final_line = f.readlines()[-1].strip()
             durations.append(float(final_line.split(",")[3]))
 
-        # load partial sim trajectory
-        cmd.load(os.path.join(sim_dir, "ready.pdb"), partial_sim)
-        cmd.load_traj(os.path.join(sim_dir, "trajectory.dcd"), partial_sim)
-
-        # select the paratope
-        for residue in paratope_residues:
-            cmd.select(
-                name=sel_on_partial,
-                selection=f"byres {partial_sim} and resi {residue[0]} and chain {residue[1]}",
-                merge=1,
-            )
-
-        rmsd_runs.append(
-            compute_rmsd_by_frame(sel_on_partial, sel_on_minimized, frames)
+        partial = mda.Universe(
+            os.path.join(sim_dir, "ready.pdb"), os.path.join(sim_dir, "trajectory.dcd")
         )
+
+        rmsd_runs.append(rms.RMSD(partial, full_sim, select=paratope_selection).run())
         rmsd_static_runs.append(
-            compute_rmsd_by_frame(sel_on_partial, sel_on_minimized, frames, True)
+            rms.RMSD(partial, minimized, select=paratope_selection).run()
         )
 
         rmsf_h_runs.append(
             compute_rmsf(
                 os.path.join(sim_dir, "ready.pdb"),
                 os.path.join(sim_dir, "trajectory.dcd"),
-                paratope_hc_residues,
+                paratope_hc_selection,
             ),  # heavy chain RMSF
         )
         rmsf_l_runs.append(
             compute_rmsf(
                 os.path.join(sim_dir, "ready.pdb"),
                 os.path.join(sim_dir, "trajectory.dcd"),
-                paratope_lc_residues,
+                paratope_lc_selection,
             ),  # heavy chain RMSF
         )
 
-        # cmd.delete(sel_on_partial)
-
-    # cmd.delete(partial_sim)
-
     # compute averages over multiple runs
     avg_duration = sum(durations) / len(durations)
-    avg_rmsd = average_rmsd_by_frame(rmsd_runs, frames)
-    avg_rmsd_static = average_rmsd_by_frame(rmsd_static_runs, frames)
+    avg_rmsd = average_rmsd(rmsd_runs, frames)
+    avg_rmsd_static = average_rmsd(rmsd_static_runs, frames)
     avg_rmsf_h = average_rmsf(rmsf_h_runs)
     avg_rmsf_l = average_rmsf(rmsf_l_runs)
+
+
+    cmd.load(os.path.join(sim_dirs[0], "ready.pdb"), f"sim_d{depth}")
+    cmd.load(os.path.join(sim_dirs[0], "trajectory.dcd"), f"sim_d{depth}")
 
     return (
         avg_duration,
@@ -206,78 +191,101 @@ def compare_sims(molecule_name: str, n_frames_str: str):
     molecule_dir = os.path.join(base, molecule_name)
 
     avg_durations = []
-    avg_rmsd_by_depth_by_frame = []
-    avg_rmsd_by_depth_by_frame_static = []
+    avg_rmsd_by_depth = []
+    avg_rmsd_by_depth_static = []
     avg_rmsf_h_by_depth = []
     avg_rmsf_l_by_depth = []
-    # rmsf_full = []
     str_depths = []
 
-    full_sim = "full_sim"
-    cmd.load(os.path.join(molecule_dir, "minimized.pdb"), full_sim)
-    cmd.load_traj(os.path.join(molecule_dir, "full_trajectory.dcd"), full_sim)
+    full_sim = minimized = mda.Universe(
+        os.path.join(molecule_dir, "minimized.pdb"),
+        os.path.join(molecule_dir, "full_trajectory.dcd"),
+    )
 
-    minimized = "minimized"
-    cmd.load(os.path.join(molecule_dir, "minimized.pdb"), minimized)
+    minimized = mda.Universe(os.path.join(molecule_dir, "minimized.pdb"))
 
     with open(os.path.join(molecule_dir, "paratope.json")) as f:
         sim_metadata = json.load(f)
         paratope_residues = sim_metadata["paratope"]
 
-    sel_on_full_sim = "paratope_on_full_sim"
-    sel_on_minimized = "paratope_on_minimized"
-    for residue in paratope_residues:
-        cmd.select(
-            name=sel_on_full_sim,
-            selection=f"{full_sim} and resi {residue[0]} and chain {residue[1]}",
-            merge=1,
+    paratope_hc_selection = (
+        "name CA and ("
+        + " or ".join(
+            [
+                f"(resid {res} and chainID {chain})"
+                for res, chain in paratope_residues
+                if chain == "H"
+            ]
         )
+        + ")"
+    )
+    paratope_lc_selection = (
+        "name CA and ("
+        + " or ".join(
+            [
+                f"(resid {res} and chainID {chain})"
+                for res, chain in paratope_residues
+                if chain == "L"
+            ]
+        )
+        + ")"
+    )
 
-        cmd.select(
-            name=sel_on_minimized,
-            selection=f"{minimized} and resi {residue[0]} and chain {residue[1]}",
-            merge=1,
+    paratope_selection = (
+        "name CA and ("
+        + " or ".join(
+            [f"(resid {res} and chainID {chain})" for res, chain in paratope_residues]
         )
+        + ")"
+    )
 
     # compute the rmsd and simulation duration for the various partial simulations
     dirs = [
         f.path for f in os.scandir(molecule_dir) if f.is_dir() and f.name != "ignore"
     ]
-    # we assume that the directory name is the neighbourhood depth used for simulations
-    str_depths += [os.path.basename(dir) for dir in dirs]
     for i, dir in enumerate(dirs):
+        # we assume that the directory name corresponds to the simulation depth
+        depth = os.path.basename(dir)
+        str_depths.append(depth)
         (
             avg_duration,
-            avg_rmsd_by_frame,
-            avg_rmsd_by_frame_static,
+            avg_rmsd,
+            avg_rmsd_static,
             avg_rmsf_h,
             avg_rmsf_l,
         ) = process_directory(
-            sel_on_full_sim,
+            full_sim,
+            minimized,
             dir,
-            int(str_depths[i]),
-            paratope_residues,
+            int(depth),
+            paratope_selection,
+            paratope_hc_selection,
+            paratope_lc_selection,
             n_frames,
         )
         avg_durations.append(avg_duration)
-        avg_rmsd_by_depth_by_frame.append(avg_rmsd_by_frame)
-        avg_rmsd_by_depth_by_frame_static.append(avg_rmsd_by_frame_static)
+        avg_rmsd_by_depth.append(avg_rmsd)
+        avg_rmsd_by_depth_static.append(avg_rmsd_static)
         avg_rmsf_h_by_depth.append(avg_rmsf_h)
         avg_rmsf_l_by_depth.append(avg_rmsf_l)
 
-    # compute the rmsd for the paratope on the full simulation
-    # we need a copy because we can't align an object to itself
-    rmsd_by_frame_static_full = compute_rmsd_by_frame(
-        sel_on_full_sim, sel_on_minimized, n_frames, True
-    )
-    avg_rmsd_by_depth_by_frame_static.append(rmsd_by_frame_static_full)
+    rmsd_static_full = rms.RMSD(full_sim, minimized, select=paratope_selection).run()
+    avg_rmsd_by_depth_static.append(average_rmsd([rmsd_static_full], n_frames))
 
     # compute the rmsf for the paratope on the full simulation
-    # rmsf_full.append(compute_rmsf(
-    #     os.path.join(molecule_dir, "minimized.pdb"),
-    #     os.path.join(molecule_dir, "full_trajectory.dcd"),
-    #     paratope_residues,
-    # ))
+    rmsf_h_full = compute_rmsf(
+        os.path.join(molecule_dir, "minimized.pdb"),
+        os.path.join(molecule_dir, "full_trajectory.dcd"),
+        paratope_hc_selection,
+    )
+    avg_rmsf_h_by_depth.append(average_rmsf([rmsf_h_full]))
+
+    rmsf_l_full = compute_rmsf(
+        os.path.join(molecule_dir, "minimized.pdb"),
+        os.path.join(molecule_dir, "full_trajectory.dcd"),
+        paratope_lc_selection,
+    )
+    avg_rmsf_l_by_depth.append(average_rmsf([rmsf_l_full]))
 
     # get the time needed for the full simulation
     with open(os.path.join(molecule_dir, "full_sim_state.csv")) as f:
@@ -286,13 +294,16 @@ def compare_sims(molecule_name: str, n_frames_str: str):
 
     str_depths.append("Whole molecule")
 
+    cmd.load(os.path.join(molecule_dir, "minimized.pdb"), "full_sim")
+    cmd.load(os.path.join(molecule_dir, "full_trajectory.dcd"), "full_sim")
+
     _, (rmsd_ax, rmsd_static_ax) = plt.subplots(2, 1)
     rmsd_ax.set_title("Partial VS full simulation")
     rmsd_static_ax.set_title("Simulation VS static molecule")
     # compare each frame with the static paratope
-    plot_rmsd(rmsd_static_ax, avg_rmsd_by_depth_by_frame_static, str_depths, n_frames)
+    plot_rmsd(rmsd_static_ax, avg_rmsd_by_depth_static, str_depths, n_frames)
     # compare each frame of partial simulations with those of the full simulation
-    plot_rmsd(rmsd_ax, avg_rmsd_by_depth_by_frame, str_depths, n_frames)
+    plot_rmsd(rmsd_ax, avg_rmsd_by_depth, str_depths, n_frames)
 
     _, (dur_ax, dur_with_full_ax) = plt.subplots(2, 1)
     dur_ax.set_title("Simulation duration")
@@ -301,12 +312,26 @@ def compare_sims(molecule_name: str, n_frames_str: str):
     # include the full simulation in the runtime plot
     plot_duration(dur_with_full_ax, avg_durations, str_depths)
 
-    _, rmsf_ax_h = plt.subplots()
+    fig_rmsf_h, rmsf_ax_h = plt.subplots()
+    fig_rmsf_h.subplots_adjust(bottom=0.2)
     rmsf_ax_h.set_title("RMSF of the paratope on H")
+    ax_rmsf_h = fig_rmsf_h.add_axes((0.7, 0.05, 0.1, 0.075))
+    btn_rmsf_h = Button(ax_rmsf_h, "Annotate")
+    btn_rmsf_h.on_clicked(
+        lambda _: toggle_annotation(rmsf_ax_h, avg_rmsf_h_by_depth)
+    )
+    buttons.append(btn_rmsf_h)
     plot_rmsf(rmsf_ax_h, avg_rmsf_h_by_depth, str_depths)
 
-    _, rmsf_ax_l = plt.subplots()
+    fig_rmsf_l, rmsf_ax_l = plt.subplots()
+    fig_rmsf_l.subplots_adjust(bottom=0.2)
     rmsf_ax_l.set_title("RMSF of the paratope on L")
+    ax_rmsf_l = fig_rmsf_l.add_axes((0.7, 0.05, 0.1, 0.075))
+    btn_rmsf_l = Button(ax_rmsf_l, "Annotate")
+    btn_rmsf_l.on_clicked(
+        lambda _: toggle_annotation(rmsf_ax_l, avg_rmsf_l_by_depth)
+    )
+    buttons.append(btn_rmsf_l)
     plot_rmsf(rmsf_ax_l, avg_rmsf_l_by_depth, str_depths)
 
     # _, rmsf_ax_full = plt.subplots()
