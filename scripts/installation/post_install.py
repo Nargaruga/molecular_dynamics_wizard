f53deb935def90fec3afdf77604201086814355a
Leonardo Da Pozzo <l.dapozzo@studenti.unipi.it>
2025-04-11 00:45:45 +0200

    use install_wizard executable

--- scripts/installation/post_install.py
+++ scripts/installation/post_install.py
@@ -1,20 +1,12 @@
 import os
 import sys
 import subprocess
-import shutil
 
 
 def main():
     wizard_root = sys.argv[1]
     env_name = sys.argv[2]
 
-    installer_path = os.path.join(
-        wizard_root,
-        "ext",
-        "pymol_wizard_installer",
-        "pymol_wizard_installer",
-        "install_wizard.py",
-    )
     paratope_wizard_root = os.path.join(wizard_root, "ext", "paratope_heatmap_wizard")
 
     if os.name == "nt":
@@ -28,11 +20,10 @@ def main():
             + [
                 "conda",
                 "run",
+                "--name",
+                "base",
                 "--no-capture-output",
-                "-n",
-                env_name,
-                "python",
-                installer_path,
+                "install_wizard",
                 paratope_wizard_root,
                 env_name,
             ],
